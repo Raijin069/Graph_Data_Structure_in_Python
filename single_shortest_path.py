9e99d511b2641ddb0cc2ca3943b71268d3a9b570
Aseem Gupta <aseemguptajaipur@gmail.com>
2023-02-14 17:23:22 +0530

    completed shortest distance file

--- single_shortest_path.py
+++ single_shortest_path.py
@@ -1,9 +1,11 @@
 def sssP(graph,node,d,distance,par):
     distance[node]  = d
+    for child in graph[node]:
+        if child != par:
+            sssP(graph,child,distance[node]+1,distance,node)
 
-edges = []
-nodes = []    
-
+edges = [['A','B'],['A','C'],['C','E'],['C','F'],['B','D'],['D','G'],['G','H'],['G','I']]
+nodes = ["A","B","C","D","E","F","G","H","I"]    
 graph = {}
 distance = {}
 for key in nodes:
@@ -17,4 +19,6 @@ for (u,v) in edges:
 start = "A"
 distance[start] = 0
 sssP(graph,start,0,distance,-1)
-print(graph)    
+for key,value in distance.items():
+    print(key,value)
+# print(graph)    
